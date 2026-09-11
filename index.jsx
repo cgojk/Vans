12d40ee819284e765e3e5ce67945a68213085a08
catalina <catalinaruedaf@hotmail.com>
2026-09-11 14:05:40 +1000

    adding host dashboard income and reviews

--- index.jsx
+++ index.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Link, Routes} from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
-import Vans from './pages/Vans.jsx';
-import VanDetail from './pages/VanDetail.jsx';
+import Vans from './pages/Vans/Vans.jsx';
+import VanDetail from './pages/Vans/VanDetail.jsx';
 import Layout from './components/Layout.jsx';
+import HostLayout from './components/HostLayout.jsx';
+import Income from './pages/Host/Income.jsx';
+import Reviews from './pages/Host/Reviews.jsx';
+import Dashboard from './pages/Host/Dashboard.jsx';
+
+
 
 
 
@@ -19,10 +25,20 @@ function App() {
     
       <Routes>
        <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<VanDetail />} />
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          
+
+
+
+            <Route path="host" element={<HostLayout />} >
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
         </Route>
       </Routes>
     </BrowserRouter>
