6e35d6ec7ac1a183e59cb4c620d7a46d2abdfe3e
catalina <catalinaruedaf@hotmail.com>
2026-09-11 12:07:37 +1000

    ading layout nested route to the page

--- index.jsx
+++ index.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Vans from './pages/Vans.jsx';
 import VanDetail from './pages/VanDetail.jsx';
+import Layout from './components/Layout.jsx';
 
 
 
@@ -15,21 +16,14 @@ import "./server"
 function App() {
   return (
     <BrowserRouter>
-     <header>
-      <Link className="site-logo" to="/">
-        #VANLIFE
-      </Link>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-      </nav>
-     </header>
+    
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+       <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
