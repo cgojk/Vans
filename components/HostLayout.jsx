af3ddb81a3ac9973b1a34d26a9b5b7b17a3ec073
catalina <catalinaruedaf@hotmail.com>
2026-09-14 11:35:23 +1000

    adding nested routes van details using . path

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -12,7 +12,7 @@ export default function HostLayout() {
         <>
             <nav className="host-nav">
                 <NavLink
-                    to="/host"
+                    to="."
                     end
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
@@ -20,14 +20,14 @@ export default function HostLayout() {
                 </NavLink>
 
                 <NavLink
-                    to="/host/income"
+                    to="income"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Income
                 </NavLink>
 
                 <NavLink
-                    to="/host/reviews"
+                    to="reviews"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Reviews
@@ -35,7 +35,7 @@ export default function HostLayout() {
 
                 
                 <NavLink
-                    to="/host/vans"
+                    to="vans"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
